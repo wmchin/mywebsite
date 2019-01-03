be2d71ca6bb7bc0e941115cc19294eba1f0feb0b
Wye Mun Chin <wyemun@Wyes-MBP.attlocal.net>
2019-01-02 17:45:27 -0800

    Updates

---
+++ precache-manifest.f31efb403522167aea632ed22e75c6fc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "49ad0a065594796add2c",
+    "url": "/mywebsite/static/css/main.3bc877a6.chunk.css"
+  },
+  {
+    "revision": "49ad0a065594796add2c",
+    "url": "/mywebsite/static/js/main.49ad0a06.chunk.js"
+  },
+  {
+    "revision": "ad953bacb427bbf53ad4",
+    "url": "/mywebsite/static/js/1.ad953bac.chunk.js"
+  },
+  {
+    "revision": "acc54b3174e7cd2ff196",
+    "url": "/mywebsite/static/js/runtime~main.acc54b31.js"
+  },
+  {
+    "revision": "9c465954abe89c801024e3f40ba2ce38",
+    "url": "/mywebsite/static/media/PSF1.9c465954.png"
+  },
+  {
+    "revision": "f886915e9ff2f6d4679069d507d9e6d1",
+    "url": "/mywebsite/static/media/Portrait.f886915e.jpg"
+  },
+  {
+    "revision": "0e1de4b04f3ddd761197733e4bce1fbb",
+    "url": "/mywebsite/static/media/MintRect.0e1de4b0.png"
+  },
+  {
+    "revision": "d548875163ba17911950ccc0227ad092",
+    "url": "/mywebsite/static/media/TelurInfo.d5488751.png"
+  },
+  {
+    "revision": "56aa9ab8e7dd2b607a64919933a45e53",
+    "url": "/mywebsite/static/media/Storyboard1.56aa9ab8.png"
+  },
+  {
+    "revision": "b3e81f7174579cfb22d2d257171fac45",
+    "url": "/mywebsite/static/media/Storyboard2.b3e81f71.png"
+  },
+  {
+    "revision": "3fd4ea4acc33d98470208debe40f880a",
+    "url": "/mywebsite/static/media/Storyboard3.3fd4ea4a.png"
+  },
+  {
+    "revision": "187a7e5ec05238290bb62fed51810d86",
+    "url": "/mywebsite/static/media/telurpaper.187a7e5e.png"
+  },
+  {
+    "revision": "a191e9f460848c150d3ecfe0d7d9d035",
+    "url": "/mywebsite/static/media/rekinderpaper.a191e9f4.png"
+  },
+  {
+    "revision": "f37f6149f3491a683a04a52137811c79",
+    "url": "/mywebsite/static/media/telurwire.f37f6149.png"
+  },
+  {
+    "revision": "95f31b55e4f920ea5e193a27d80a5703",
+    "url": "/mywebsite/static/media/lay1.95f31b55.png"
+  },
+  {
+    "revision": "bf3cf4f5728defd6d37ed3340e8bf3c8",
+    "url": "/mywebsite/static/media/lay2.bf3cf4f5.png"
+  },
+  {
+    "revision": "349c08cb83e8c0064e54cc5fc965b5d6",
+    "url": "/mywebsite/static/media/lay1real.349c08cb.png"
+  },
+  {
+    "revision": "f2940ae524fbb60d1ea8d9b31666e0b7",
+    "url": "/mywebsite/static/media/lay2real.f2940ae5.png"
+  },
+  {
+    "revision": "c1f672d8f46032dc4ebbba6cab94539f",
+    "url": "/mywebsite/static/media/MintInfoSS.c1f672d8.png"
+  },
+  {
+    "revision": "4e6fb494787f233a926419ff7ebc9d69",
+    "url": "/mywebsite/static/media/MintScreens.4e6fb494.png"
+  },
+  {
+    "revision": "98465ce4c28c3eeea845041ed1e6ce26",
+    "url": "/mywebsite/static/media/DCE.98465ce4.png"
+  },
+  {
+    "revision": "f0761584a04e1333175c70d23d82a50d",
+    "url": "/mywebsite/static/media/GBTGN.f0761584.png"
+  },
+  {
+    "revision": "8feda939dd717ba99edb9c888c39eb73",
+    "url": "/mywebsite/static/media/PSF2.8feda939.png"
+  },
+  {
+    "revision": "a5ff27d5e39521c765ffcbd9257bc608",
+    "url": "/mywebsite/static/media/PSF3.a5ff27d5.png"
+  },
+  {
+    "revision": "58d82c4fa1a8660d2c537a182ae2719a",
+    "url": "/mywebsite/static/media/PSF4.58d82c4f.png"
+  },
+  {
+    "revision": "ea2ecd8a9134939b4e0cda200a8d1be6",
+    "url": "/mywebsite/static/media/PSF5.ea2ecd8a.png"
+  },
+  {
+    "revision": "d86365861c957fa7ab6f345e55a9b3d5",
+    "url": "/mywebsite/static/media/PSF6.d8636586.png"
+  },
+  {
+    "revision": "96db72dd663541178ab1a0215a238274",
+    "url": "/mywebsite/static/media/PSF7.96db72dd.png"
+  },
+  {
+    "revision": "8898e303929f3370b63c258648925f3a",
+    "url": "/mywebsite/index.html"
+  }
+];
